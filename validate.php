97dc7d1efe799fba8672fcfc04a1c8cb7bf68d3c
soyyogi <raoyogesh78@yahoo.in>
2021-02-02 02:00:02 +0100

    added user auth

--- validate.php
+++ validate.php
@@ -1,3 +1,13 @@
 <?php
 $user = 'yogi';
-$password = 'abcd1234';
+$pwd = 'abcd1234';
+$username = $_POST['username'];
+$password = $_POST['password'];
+if ($user === $username && $pwd === $password) {
+    session_start();
+    $_SESSION['username'] = $_POST['username'];
+    header('Location: localhost:3000/panel.php');
+} else {
+    header('Location: localhost:3000/index.php');
+}
+
